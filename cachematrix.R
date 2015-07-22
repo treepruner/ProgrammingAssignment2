14340bbd0f32f75c2b23d9c32a94b5bfda2c7579
treepruner <tree.pruner@gmail.com>
2015-07-22 13:09:24 -0700

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Caches the inverse of a matrix and stores it in cache
+## These convoluted code is the r equivalent of the plot of the movie Inception
+## 
+## Comments are embedded inside the function
+##
+## See also https://github.com/DanieleP/PA2-clarifying_instructions
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
+        m <- NULL                                 # sets m to undefined         
+        set <- function(y) {                      # changes the vector stored in the main function
+                x <<- y                           # substitutes the vector x with y (the input) in the main function
+                m <<- NULL                        # restores m to null
         }
-        get <- function() x
-        setInverse <- function(solve) m <<- solve
-        getInverse <- function() m
+        get <- function() x                       # returns matrix x stored in main function
+        setInverse <- function(solve) m <<- solve # performs the inverse on the matrix
+        getInverse <- function() m                # returns the inverse
+                                                  # stores the functions by name in a list
         list(set = set, get = get,
              setInverse = setInverse,
              getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of a matrix if it isn't already in the cache
+## see the comments on each line
+## See also https://github.com/DanieleP/PA2-clarifying_instructions
+## 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getInverse()
-        if(!is.null(m)) {
+        ## the built in function solve gets the inverse
+        ## 
+        m <- x$getInverse()                       # returns from list defined in makeCacheMatrix
+        if(!is.null(m)) {                         # if m isn't null, it prints it out with message
                 message("getting cached data")
                 return(m)
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setInverse(m)
+        data <- x$get()                            # if m isn't found, then it brings the matrix back
+        m <- solve(data, ...)                      # computes the inverse
+        x$setInverse(m)                            # stores the inverse for next time
         m
 }
